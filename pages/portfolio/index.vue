26cd385c1971df544aa8d44ad08fcdac190ab553
somecallmejosh <josh@thebrileys.com>
2024-03-20 12:45:52 -0400

    Minor text update.

--- pages/portfolio/index.vue
+++ pages/portfolio/index.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="max-w-full p-12 py-20 prose lg:p-24">
       <h1>UX/UI Design Portfolio</h1>
-      <p>Welcome to my portfolio. I'm Joshua K. Briley, and I specialize in UI design and front-end development. Over the years, I've focused on creating clear, user-friendly, and accessible digital products. My role has often bridged the gap between design and development, working directly with both stakeholders and technical teams to ensure that the final product meets everyone's needs. Here, you'll find a selection of projects that showcase my approach to design and development, highlighting my commitment to practical, effective solutions.</p>
+      <p>Welcome to my portfolio. I'm Joshua Briley, and I specialize in UI design and front-end development. Over the years, I've focused on creating clear, user-friendly, and accessible digital products. My role has often bridged the gap between design and development, working directly with both stakeholders and technical teams to ensure that the final product meets everyone's needs. Here, you'll find a selection of projects that showcase my approach to design and development, highlighting my commitment to practical, effective solutions.</p>
     </div>
   </div>
 </template>
