e70297b79a4a0dee1e33dad921b39179d40705fa
somecallmejosh <josh@thebrileys.com>
2024-03-20 11:05:46 -0400

    Fix HTMLAttrs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,10 @@ export default defineNuxtConfig({
   app: {
     head: {
       title: 'Joshua Briley',
+      htmlAttrs: {
+        lang: 'en',
+        class: 'scroll-smooth',
+      },
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -11,5 +15,4 @@ export default defineNuxtConfig({
   },
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
-  plugins: ['~/plugins/html-class.js'],
 })
--- plugins/html-class.js
+++
@@ -1,5 +0,0 @@
-export default defineNuxtPlugin(() => {
-  if (process.client) {
-    document.documentElement.classList.add('scroll-smooth');
-  }
-});
