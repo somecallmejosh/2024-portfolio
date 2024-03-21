21ea2bac81d3984e20658022b85436a5fdc96734
somecallmejosh <josh@thebrileys.com>
2024-03-21 14:42:47 -0400

    Fix Portfolio Nav overflow.

--- components/content/LinkNewWindow.vue
+++ components/content/LinkNewWindow.vue
@@ -11,6 +11,9 @@ const props = defineProps({
 </script>
 <template>
   <p>
-    <a :href="href" target="_blank" rel="noreferrer" class="inline-flex items-center gap-1 p-1 px-2 text-sm font-medium transition-colors duration-150 rounded not-prose group bg-slate-50 hover:bg-slate-100 hover:shadow text-slate-700">Visit {{ href }} <svg role="img" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" class="h-5" viewBox="0 -960 960 960"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h560v-280h80v280q0 33-23.5 56.5T760-120H200Zm188-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z"/></svg></a>
+    <a :href="href" target="_blank" rel="noreferrer" class="inline-flex items-center max-w-full gap-2 p-1 px-2 text-sm font-medium transition-colors duration-150 rounded not-prose group bg-slate-50 hover:bg-slate-100 hover:shadow text-slate-700">
+      <span class="overflow-x-scroll grow whitespace-nowrap">Visit {{ href }}</span>
+      <svg role="img" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" class="h-5 shrink-0" viewBox="0 -960 960 960"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h560v-280h80v280q0 33-23.5 56.5T760-120H200Zm188-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z"/></svg>
+    </a>
   </p>
 </template>
