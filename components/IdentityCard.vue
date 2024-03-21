74f12d045577563f13f7461738319596660c109f
somecallmejosh <josh@thebrileys.com>
2024-03-21 14:31:49 -0400

    Update header.

--- components/IdentityCard.vue
+++ components/IdentityCard.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="bg-white  sticky top-20 pt-12 rounded-xl">
-    <div class="w-56 h-48 mx-auto bg-gray-50 -translate-y-24 rounded-2xl hover:blur transition-all duration-500 relative group">
-      <img class="w-56 h-48 flex-none rounded-2xl object-cover relative z-0" src="https://www.logatot.com/assets/team/josh-95a85dfb5617d380bc60162eafa2d6fe6413112b22c35f161d6589f11580eb34.jpg" alt="Joshua Briley, UX/UI Developer">
+  <div class="sticky pt-12 bg-white top-20 rounded-xl">
+    <div class="relative w-56 h-48 mx-auto transition-all duration-500 -translate-y-24 bg-gray-50 rounded-2xl hover:blur group">
+      <img class="relative z-0 flex-none object-cover w-56 h-48 rounded-2xl" src="https://www.logatot.com/assets/team/josh-95a85dfb5617d380bc60162eafa2d6fe6413112b22c35f161d6589f11580eb34.jpg" alt="Joshua Briley, UX/UI Developer">
     </div>
     <div class="-mt-14">
-      <div class="text-center px-4 mb-10">
-        <h2 class="text-xl font-bold mb-1">Joshua Briley</h2>
-        <p class="text-gray-500 font-medium">UI/UX Developer</p>
+      <div class="px-4 mb-10 text-center">
+        <h2 class="mb-1 text-xl font-bold">Joshua Briley</h2>
+        <p class="font-medium text-gray-500">UI/UX Developer</p>
       </div>
-      <ul class="text-left px-12 pt-10 pb-12 bg-slate-50 space-y-4 text-gray-600 font-medium rounded-b-2xl">
+      <ul class="px-12 pt-10 pb-12 space-y-4 font-medium text-left text-gray-600 bg-slate-50 rounded-b-2xl">
         <li class="flex items-center gap-2">
           <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M480-480q33 0 56.5-23.5T560-560q0-33-23.5-56.5T480-640q-33 0-56.5 23.5T400-560q0 33 23.5 56.5T480-480Zm0 294q122-112 181-203.5T720-552q0-109-69.5-178.5T480-800q-101 0-170.5 69.5T240-552q0 71 59 162.5T480-186Zm0 106Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Zm0-480Z"/></svg>
           <div class="sr-only">I live in</div>
